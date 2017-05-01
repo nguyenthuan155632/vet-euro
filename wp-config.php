ca15f43dc3cd53cf98f0829b71fb7cbd9a510cbe
vensera <nt@vensera.com>
2017-05-01 09:28:15 +0000

    change database host

--- wp-config.php
+++ wp-config.php
@@ -31,7 +31,7 @@ define('DB_USER', 'vensera');
 define('DB_PASSWORD', '15563200025ff0');
 
 /** MySQL hostname */
-define('DB_HOST', 'vensera.cisdtmhkjjzs.ap-northeast-1.rds.amazonaws.com');
+define('DB_HOST', 'venseradb.ceoze0wmec82.ap-northeast-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
